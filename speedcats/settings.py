22f506ef8323dc7e5772fa5ade7d37fada007080
ajessefowler <ajessefowler@gmail.com>
2018-06-18 21:22:56 -0400

    AWS media storage should work now

--- speedcats/settings.py
+++ speedcats/settings.py
@@ -137,4 +137,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
