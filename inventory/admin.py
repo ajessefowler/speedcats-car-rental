f1adfada12d50ddf439a9ad5d46fe3f2cb28dac8
ajessefowler <ajessefowler@gmail.com>
2018-07-08 22:58:10 -0400

    Add Profile Model
    Add profile model to extend user
    Remove 'Loaned' choice from vehicle status

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Store, Vehicle, Reservation
+from .models import Profile, Store, Vehicle, Reservation
 
+admin.site.register(Profile)
 admin.site.register(Store)
 admin.site.register(Vehicle)
 admin.site.register(Reservation)
--- inventory/models.py
+++ inventory/models.py
@@ -3,9 +3,22 @@ from django.urls import reverse
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.core.validators import RegexValidator
 
 import pytz
 import datetime
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+	# Phone regex found here: https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-phone-number-in-django-models
+	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+	phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+	address = models.CharField(max_length=100)
+	city = models.CharField(max_length=50)
+	state = models.CharField(max_length=2)
+	zipcode = models.IntegerField()
+	license_state = models.CharField(max_length=2)
 
 class Store(models.Model):
 
@@ -27,7 +40,6 @@ class Vehicle(models.Model):
 
 	status_choices = (
 		('m', 'Maintenance'),
-		('l', 'Loaned'),
 		('a', 'Available'),
 		('r', 'Reserved'),
 	)
