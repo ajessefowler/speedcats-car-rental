1304e00106b45373630006c496c515ae765929cd
ajessefowler <ajessefowler@gmail.com>
2018-07-24 13:31:24 -0400

    Fix Celery bugs

--- speedcats/__init__.py
+++ speedcats/__init__.py
@@ -5,6 +5,6 @@ from __future__ import absolute_import, unicode_literals
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery_tasks import app as celery_app
+from speedcats.celery_tasks import app as celery_app
 
 __all__ = ('celery_app',)
