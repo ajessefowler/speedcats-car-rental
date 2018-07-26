706215cc16eab7c5c6d03af47a870d69b98f8421
ajessefowler <ajessefowler@gmail.com>
2018-07-26 14:51:35 -0400

    Add menu shade

--- inventory/static/js/menu.js
+++ inventory/static/js/menu.js
@@ -1,16 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(event) {
     let menuOut = false;
-    
+		
+		document.getElementById('menushade').addEventListener('click', function() {
+			closeMenu();
+		})
+
     document.getElementById('menubutton').addEventListener('click', function() {
-		if (!menuOut) {
-			menuOut = true;
-			document.querySelector('nav').style.animation = 'menuOut .3s ease forwards';
-			document.getElementById('menubutton').innerHTML = 'chevron_left';
-		} else {
+			if (!menuOut) {
+				menuOut = true;
+				document.getElementById('menushade').style.display = 'block';
+				document.querySelector('nav').style.animation = 'menuOut .3s ease forwards';
+				document.getElementById('menushade').style.animation = 'fadeIn .3s ease forwards';
+				document.getElementById('menubutton').innerHTML = 'chevron_left';
+			} else {
+				closeMenu();
+			}
+    });
+		
+		function closeMenu() {
 			menuOut = false;
 			document.querySelector('nav').style.animation = 'menuIn .3s ease forwards';
+			document.getElementById('menushade').style.animation = 'fadeOut .3s ease forwards';
 			document.getElementById('menubutton').innerHTML = 'menu';
+			setTimeout(function() {
+				document.getElementById('menushade').style.display = 'none';
+			}, 300);
 		}
-    });
-    
 });
